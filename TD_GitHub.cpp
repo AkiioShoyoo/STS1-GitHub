08474d4242a159a5e69d24f467e49c95a8f5217e
AkiioShoyoo <cantin42660@gmail.com>
2023-04-07 08:39:56 +0200

    Update TP_Github.cpp
    Ajout des fonctions exposant, plusGrand, estMajeur et TVA

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,10 +1,45 @@
 //Fonctions réalisées par l'étudiant 1 :
 
+bool estMajeur(int age)
+{
+    if(age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+int exposant(int nbre, int exp)
+{
+   int resultat;
 
+	resultat = nbre * exp;
+  
+	return resultat;
+}
 
+float TVA(int prix)
+{
+    float prix_ttc = prix * 1.196f;
+    return prix_ttc; 
+}
 
+int plusGrand(int a, int b, int c)
+{
+    int max=a;
 
+    if(b > max)
+    {
+        max = b;
+    }
+
+    if(c > max)
+    {
+        max = c;
+    }
+
+    return max;
+}
 
 
 
